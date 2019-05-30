2042242e9b7d40d3d4962e4304f7d37e279c5303
Christine Bussinger <christinebussingerdev@gmail.com>
2019-05-30 19:21:35 -0400

    added pm class, finished project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,6 +14,7 @@ class Person {
 
 class Instructor extends Person {
     constructor(instrucAttrs) {
+        super(instrucAttrs);
         this.specialty = instrucAttrs.specialty;
         this.favLanguage = instrucAttrs.favLanguage;
         this.catchPhrase = instrucAttrs.catchPhrase;
@@ -30,6 +31,7 @@ class Instructor extends Person {
 
 class Student extends Person {
     constructor(studentAttrs) {
+        super(studentAttrs);
         this.previousBackground = studentAttrs.previousBackground;
         this.className = studentAttrs.className;
         this.favSubjects = studentAttrs.favSubjects;
@@ -46,4 +48,21 @@ class Student extends Person {
     sprintChallenge() {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttrs) {
+        super(pmAttrs);
+        this.gradClassName = pmAttrs.gradClassName;
+        this.favInstructor = pmAttrs.favInstructor;
+    }
+
+    standUp(slackChannel) {
+        console.log(`${this.name} announces to ${slackChannel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student}'s code on ${subject}`);
+    }
+
 }
