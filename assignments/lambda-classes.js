72ab184b2109ce327663a33e8b37c8b4b9c10c24
Christine Bussinger <christinebussingerdev@gmail.com>
2019-05-30 19:16:11 -0400

    added person, instructor, and student classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,49 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(personAttrs) {
+        this.name = personAttrs.name;
+        this.age = personAttrs.age;
+        this.location = personAttrs.location;
+    }
+
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+    }
+
+}
+
+class Instructor extends Person {
+    constructor(instrucAttrs) {
+        this.specialty = instrucAttrs.specialty;
+        this.favLanguage = instrucAttrs.favLanguage;
+        this.catchPhrase = instrucAttrs.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
+    }
+    
+    listsSubjects() {
+        this.favSubjects.foreach((subject) => {console.log(subject)})
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+
+    sprintChallenge() {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
